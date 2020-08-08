00a6a98cca7aaa370cad2162a75f841b7fb0b912
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2020-08-08 11:24:48 +0200

    Update scripts.js
    This file contains all functions to make examination app work as expected

--- scripts.js
+++ scripts.js
@@ -1,1 +1,127 @@
 
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+
+const questionElement = document.getElementById('question')
+
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+    //console.log('Started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if(answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while(answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if(shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText= 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if(correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+} 
+
+
+const questions = [
+    {
+        question: '7 + 2= ?',
+        answer: [
+            { text: '7', correct: false },
+            { text: '9', correct: true }
+            { text: '22', correct: false }
+        ]
+    },
+    {
+        question: 'Who is the best youtuber?',
+        answers: [
+            {text: 'Web Dev', correct: true},
+            {text: 'Traversy Media', correct: true},
+            {text: 'Harry Mack', correct: true},
+            {text: 'Web Hacking', correct: false},
+            {text: 'Gadrawingz', correct: true},
+            {text: 'Dev Ed', correct: true},
+        ]
+    },
+
+    {
+        question: 'Who is the founder of Facebook?',
+        answers: [
+            {text: 'Henry', correct: false},
+            {text: 'Bill Gates', correct: false},
+            {text: 'Mark Z', correct: true},
+            {text: 'Adams', correct: false},
+        ]
+    },
+
+    {
+        question: '8 * 2= ?',
+        answers: [
+            {text: '16', correct: false},
+            {text: '8', correct: true}
+        ]
+    }
+]
